89ddec0f845633e60321d15249afdc3ca711aacc
Hsiang-Chieh Tsou <hsjts0u@gmail.com>
2023-11-19 00:06:45 -0800

    Basic game of life demo using a loop and putStrLn

--- app/Main.hs
+++ app/Main.hs
@@ -2,10 +2,18 @@ module Main (main) where
 
 import qualified Life       as L
 import qualified PresetGrid as P
+import Control.Concurrent (threadDelay)
 
 main :: IO ()
 main = do 
-    L.visualize (P.strToGrid P.boat) L.gridRows L.gridCols
+    let initialGrid = P.strToGrid P.beacon
+    loop initialGrid
+
+loop :: L.GridState -> IO ()
+loop currentSt = do
+    let newSt = L.evolution currentSt
+    L.visualize newSt L.gridRows L.gridCols
     putStrLn ""
-    L.visualize (L.evolution (P.strToGrid P.boat)) L.gridRows L.gridCols
+    threadDelay 1000000
+    loop newSt
     
