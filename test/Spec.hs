8c3f2086cea7d320717b0c98c789e5d9d0483c3d
Ting-Yang Chou <131313678+t3chou@users.noreply.github.com>
2023-12-15 05:31:27 +0800

    Update Spec.hs

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,69 +1,78 @@
 -- QuickCheck Test for Conway's Game of Life
-
-module Spec where
+module Main where
+-- module Spec where
 
 import Test.QuickCheck
-import Life
+import qualified Life as L
 import PresetGrid
 import qualified Data.Array as A
 
+instance Arbitrary L.CellState where
+    arbitrary = elements [L.Alive, L.Dead]
+
+instance Arbitrary L.GridState where
+    arbitrary = do
+        states <- vectorOf (L.gridRows * L.gridCols) arbitrary
+        return $ L.GridState (A.listArray ((0, 0), (L.gridRows - 1, L.gridCols - 1)) states)
+
 -- Helper function to count live neighbors
-liveNeighbors :: GridState -> GridIndex -> Int
-liveNeighbors (GridState grid) (x, y) = length $ filter isLive neighbors
+liveNeighbors :: L.GridState -> L.GridIndex -> Int
+liveNeighbors (L.GridState grid) (x, y) = length $ filter isLive neighbors
   where
     neighbors = [(i, j) | i <- [x-1..x+1], j <- [y-1..y+1], (i, j) /= (x, y)]
     isLive idx = case grid A.! idx of
-                    Alive -> True
+                    L.Alive -> True
                     _     -> False
 
 -- Property to test if a dead cell with exactly three live neighbors becomes alive
-prop_birthRule :: GridState -> GridIndex -> Property
+prop_birthRule :: L.GridState -> L.GridIndex -> Property
 prop_birthRule initialState idx = 
     within 10000 $
-    (grid A.! idx == Dead && liveNeighbors initialState idx == 3) ==>
-    let newState = evolution initialState in
-    (newState `getCellValue` idx) == Alive
+    (grid A.! idx == L.Dead && liveNeighbors initialState idx == 3) ==>
+    let newState = L.evolution initialState in
+    (newState `getCellValue` idx) == L.Alive
   where
     grid = getGrid initialState
 
 -- Property to test survival rule
-prop_survivalRule :: GridState -> GridIndex -> Property
+prop_survivalRule :: L.GridState -> L.GridIndex -> Property
 prop_survivalRule initialState idx = 
     within 10000 $
-    (grid A.! idx == Alive && liveNeighbors initialState idx `elem` [2, 3]) ==>
-    let newState = evolution initialState in
-    (newState `getCellValue` idx) == Alive
+    (grid A.! idx == L.Alive && liveNeighbors initialState idx `elem` [2, 3]) ==>
+    let newState = L.evolution initialState in
+    (newState `getCellValue` idx) == L.Alive
   where
     grid = getGrid initialState
 
 -- Property to test overpopulation and underpopulation rules
-prop_deathRule :: GridState -> GridIndex -> Property
+prop_deathRule :: L.GridState -> L.GridIndex -> Property
 prop_deathRule initialState idx = 
     within 10000 $
-    (grid A.! idx == Alive && (liveNeighbors initialState idx < 2 || liveNeighbors initialState idx > 3)) ==>
-    let newState = evolution initialState in
-    (newState `getCellValue` idx) == Dead
+    (grid A.! idx == L.Alive && (liveNeighbors initialState idx < 2 || liveNeighbors initialState idx > 3)) ==>
+    let newState = L.evolution initialState in
+    (newState `getCellValue` idx) == L.Dead
   where
     grid = getGrid initialState
 
--- Helper function to get the cell value from a GridState
-getCellValue :: GridState -> GridIndex -> CellState
-getCellValue (GridState grid) idx = grid A.! idx
+-- Helper function to get the cell value from a L.GridState
+getCellValue :: L.GridState -> L.GridIndex -> L.CellState
+getCellValue (L.GridState grid) idx = grid A.! idx
 
--- Helper function to get the grid from a GridState
-getGrid :: GridState -> A.Array GridIndex CellState
-getGrid (GridState grid) = grid
+-- Helper function to get the grid from a L.GridState
+getGrid :: L.GridState -> A.Array L.GridIndex L.CellState
+getGrid (L.GridState grid) = grid
 
 -- Tests for preset grids
 prop_presetGrids :: Property
 prop_presetGrids = 
     within 10000 $
-    conjoin [testPresetGrid block, testPresetGrid beehive, testPresetGrid loaf, 
-             testPresetGrid boat, testPresetGrid tub, testPresetGrid blinker,
-             testPresetGrid toad, testPresetGrid beacon]
+    conjoin [testPresetGrid (strToGrid block), testPresetGrid (strToGrid beehive), testPresetGrid (strToGrid loaf), 
+             testPresetGrid (strToGrid boat), testPresetGrid (strToGrid tub), testPresetGrid (strToGrid blinker),
+             testPresetGrid (strToGrid toad), testPresetGrid (strToGrid beacon)]
 
-testPresetGrid :: GridState -> Bool
-testPresetGrid gridState = evolution gridState == gridState  -- Preset grids should remain unchanged after one evolution
+
+testPresetGrid :: L.GridState -> Bool
+testPresetGrid (L.GridState grid) = L.evolution (L.GridState grid) == (L.GridState grid)
 
 -- Main function to run all tests
 main :: IO ()
