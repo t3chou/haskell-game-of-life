c94a0892d5b23d070647970ee2e9e4853d817c57
Andersonabc <peter89119@gmail.com>
2023-12-14 17:27:50 -0800

    Remove TestState.hs. Integrate into Spec.hs

--- test/Spec.hs
+++ test/Spec.hs
@@ -4,6 +4,7 @@ module Main where
 
 import Test.QuickCheck
 import qualified Life as L
+import qualified PresetGrid as P 
 import PresetGrid
 import qualified Data.Array as A
 
@@ -74,11 +75,152 @@ prop_presetGrids =
 testPresetGrid :: L.GridState -> Bool
 testPresetGrid (L.GridState grid) = L.evolution (L.GridState grid) == (L.GridState grid)
 
+
+
+tester :: String -> String
+tester grid = do 
+    let initialGrid = P.strToGrid grid
+        newState = L.evolution initialGrid
+    gridToString newState L.gridRows L.gridCols
+
+-- Use this for testing
+gridToString :: L.GridState -> Int -> Int -> String
+gridToString (L.GridState g) rs cs =
+    let cellChar L.Alive = 'o'
+        cellChar L.Dead  = '.'
+        rowToString r = [cellChar (g A.! (r, c)) | c <- [0..cs-1]] -- ++ "\n"
+    in concat [rowToString r | r <- [0..rs-1]]
+
+
+-- Property: The number of cells in the grid remains constant after evolution.
+prop_evolutionConservesCells :: L.GridState -> Bool
+prop_evolutionConservesCells grid =
+    let L.GridState arr = L.evolution grid
+    in length (A.assocs arr) == L.gridRows * L.gridCols
+
+
+
 -- Main function to run all tests
 main :: IO ()
 main = do
   putStrLn "Testing Conway's Game of Life"
-  quickCheck prop_birthRule
-  quickCheck prop_survivalRule
-  quickCheck prop_deathRule
-  quickCheck prop_presetGrids
+--   quickCheck prop_birthRule
+--   quickCheck prop_survivalRule
+  -- quickCheck prop_deathRule
+  -- quickCheck prop_presetGrids
+  quickCheck prop_evolutionConservesCells
+  putStrLn $ if block2 == tester P.block then "OK" else "FAIL!"
+  putStrLn $ if beehive2 == tester P.beehive then "OK" else "FAIL!"
+  putStrLn $ if loaf2 == tester P.loaf then "OK" else "FAIL!"
+  putStrLn $ if boat2 == tester P.boat then "OK" else "FAIL!"
+  putStrLn $ if tub2 == tester P.tub then "OK" else "FAIL!"
+  putStrLn $ if blinker2 == tester P.blinker  then "OK" else "FAIL!"
+  putStrLn $ if toad2 == tester P.toad  then "OK" else "FAIL!"
+  putStrLn $ if beacon2 == tester P.beacon then "OK" else "FAIL!"
+
+-- | New State (Still Life)
+block2 :: String
+block2 = "\
+    \..........\
+    \..........\
+    \..........\
+    \..........\
+    \....oo....\
+    \....oo....\
+    \..........\
+    \..........\
+    \..........\
+    \.........."
+
+beehive2 :: String
+beehive2 = "\
+    \..........\
+    \..........\
+    \..........\
+    \..........\
+    \....oo....\
+    \...o..o...\
+    \....oo....\
+    \..........\
+    \..........\
+    \.........."
+
+loaf2 :: String
+loaf2 = "\
+    \..........\
+    \..........\
+    \..........\
+    \....oo....\
+    \...o..o...\
+    \....o.o...\
+    \.....o....\
+    \..........\
+    \..........\
+    \.........."
+
+boat2 :: String
+boat2 = "\
+    \..........\
+    \..........\
+    \..........\
+    \..........\
+    \...oo.....\
+    \...o.o....\
+    \....o.....\
+    \..........\
+    \..........\
+    \.........."
+
+tub2 :: String
+tub2 = "\
+    \..........\
+    \..........\
+    \..........\
+    \..........\
+    \....o.....\
+    \...o.o....\
+    \....o.....\
+    \..........\
+    \..........\
+    \.........."
+
+
+-- | Oscillators
+blinker2 :: String
+blinker2 = "\
+    \..........\
+    \..........\
+    \..........\
+    \..........\
+    \..........\
+    \...ooo....\
+    \..........\
+    \..........\
+    \..........\
+    \.........."
+
+toad2 :: String
+toad2 = "\
+    \..........\
+    \..........\
+    \..........\
+    \..........\
+    \....ooo...\
+    \...ooo....\
+    \..........\
+    \..........\
+    \..........\
+    \.........."
+
+beacon2 :: String
+beacon2 = "\
+    \..........\
+    \..........\
+    \..........\
+    \...oo.....\
+    \...o......\
+    \......o...\
+    \.....oo...\
+    \..........\
+    \..........\
+    \.........."
